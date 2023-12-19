1224ddd1e5443f6b26e83b05c232535fdbf51a55
yanmaaa <gxabyan17@gmail.com>
2023-12-20 01:54:39 +0700

    second commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Mendapatkan nama dari pengguna
+var nama = prompt("Masukkan nama Anda:");
+
+// Membuat pesan sambutan dengan nama yang diisi oleh pengguna
+var pesanSambutan = "Selamat datang, " + nama + "! Terima kasih sudah bergabung.";
+
+// Menampilkan pesan sambutan
+console.log(pesanSambutan);
