d01cd9b9d89c17880c77e5979163c384d98d6da2
yanmaaa <gxabyan17@gmail.com>
2023-12-22 03:42:05 +0700

    five commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-// var userName = prompt("Please enter your name:");
-
-// if (userName) {
-// document.getElementById("siapa").textContent = userName;
-// }
+var userName = prompt("Please enter your name:");
+if (userName) {
+document.getElementById("siapa").textContent = userName;
+}
 
 var myIndex = 0;
 carousel();
-
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -18,3 +16,56 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 5000);
 }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function validateForm() {
+  var nama = document.getElementById("nama").value;
+  var birth = document.getElementById("birth").value;
+  var male = document.getElementById("male").checked;
+  var female = document.getElementById("female").checked;
+  var sent = document.getElementById("sent").value;
+
+  if (nama === "" || birth === "" || (!male && !female) || sent === "") {
+    alert("Mohon lengkapi semua input!");
+    return false;
+  }
+
+  return true;
+}
+
+function resetForm() {
+  document.getElementById("form").reset();
+}
+
+document.getElementById("form").addEventListener("submit", function(event) {
+  if (!validateForm()) {
+    event.preventDefault();
+  }else{
+    resetForm();
+  }
+});
